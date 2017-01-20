a6b859a551b59e467dec48207abaa323c2a4e4ff
ArtemSharypov <artem.sharypov@gmail.com>
2017-01-19 18:28:46 -0600

    Delete APIHolder.java

--- app/src/main/java/com/artem/weather/APIHolder.java
+++
@@ -1,7 +0,0 @@
-package com.artem.weather;
-
-
-public class APIHolder {
-    public final static String WG_API_KEY = "4b02eb9b5cdccbf8";
-    public final static String GOOGLE_API_KEY = "AIzaSyBG8ZnSnQEi776LRfzjfXcwD3xA_xVMcRc";
-}
